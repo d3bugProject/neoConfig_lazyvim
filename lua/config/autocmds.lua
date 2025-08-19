3aaa02174ed41267a68ba745b938815412762921
d3bugProject <d3bugalg@gmail.com>
2025-08-19 16:33:15 +0100

    alors je vais essyer de performer ma config perso sur une config a base de lazy vim

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,3 +5,19 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     vim.cmd("TSToolsOrganizeImports")
   end,
 })
+
+-- Génération auto de jsconfig.json à l'ouverture d'un fichier JS/TS
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+  callback = function()
+    local jsconfig = vim.fn.getcwd() .. "/jsconfig.json"
+    if vim.fn.filereadable(jsconfig) == 0 then
+      local config = {
+        compilerOptions = {},
+        exclude = { "dist" },
+      }
+      local json = vim.fn.json_encode(config)
+      vim.fn.writefile({ json }, jsconfig)
+    end
+  end,
+})
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,38 +1,33 @@
 return {
-  "hrsh7th/nvim-cmp",
-  opts = function(_, opts)
-    local cmp = require("cmp")
-
-    opts.preselect = cmp.PreselectMode.None
-    opts.completion = opts.completion or {}
-    opts.completion.completeopt = "menu,menuone,noselect"
-
-    opts.mapping = {
-      ["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-      ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-      ["<CR>"] = cmp.mapping(function(fallback)
-        if cmp.visible() and cmp.get_active_entry() then
-          cmp.confirm({ select = false })
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-      ["<C-Space>"] = cmp.mapping.complete(),
-      ["<Esc>"] = cmp.mapping.abort(),
-    }
-
-    return opts
-  end,
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      preset = "none",
+      ["<Tab>"] = { "select_next", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "fallback" },
+      ["<CR>"] = { "accept", "fallback" },
+      ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<Esc>"] = { "hide", "fallback" },
+    },
+    completion = {
+      list = {
+        selection = {
+          preselect = false, -- Pas de présélection
+          auto_insert = false, -- Pas d'insertion automatique
+        },
+      },
+      menu = {
+        draw = {
+          treesitter = { "lsp" },
+        },
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 200,
+      },
+      ghost_text = {
+        enabled = vim.g.ai_cmp,
+      },
+    },
+  },
 }
