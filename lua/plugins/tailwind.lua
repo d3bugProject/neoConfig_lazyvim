e8202014188e25341d93f05cb41fc91882be2487
blacklight <d3bugalg@gmail.com>
2025-11-26 09:37:48 +0100

    snipets

--- lua/plugins/tailwind.lua
+++ lua/plugins/tailwind.lua
@@ -7,6 +7,9 @@ return {
 					filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact", "html", "css", "svelte" },
 					-- Pour Tailwind v4, ajoute ici des settings spécifiques si besoin
 				},
+				cssls = {
+					filetypes = { "css", "scss", "less" },
+				},
 			},
 		},
 	},
